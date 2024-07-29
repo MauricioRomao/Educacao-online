7d09c49266cc7e009773ab3eb452acd4ef948343
MauricioRomao <devmauroromao@gmail.com>
2024-07-29 22:16:15 +0100

    Pegando dados do formulario

--- front-End/kalabo_internacional/src/components/Login.jsx
+++ front-End/kalabo_internacional/src/components/Login.jsx
@@ -1,21 +1,47 @@
 import { FaUser, FaLock } from 'react-icons/fa';
+import { useState } from 'react';
+
+
 
 
 function Login_content (){
+
+
+  function FormAction(e){
+  
+    e.preventDefault()
+    
+    console.log(`User ${name}` )
+    console.log(`Pass ${pass}` )
+    
+
+  }
+
+  const [name, Setname] = useState()
+  const [pass, Setpass] = useState()
+  
+
+
+
+
     return (
         <div>
 
-<form action="post" method="">
+<form action="post" method="" onSubmit={FormAction}>
           <div className="input-group">
             <div className="input-wrapper">
               <FaUser className="icon" />
-              <input type="text" name="nome" id="nome" placeholder="Digite o seu nome" />
+              <input type="text" name="nome" id="nome" 
+              onChange={(e)=> Setname(e.target.value) }
+              placeholder="Digite o seu nome" />
             </div>
           </div>
           <div className="input-group">
             <div className="input-wrapper">
               <FaLock className="icon" />
-              <input type="password" name="pass" id="pass" placeholder="Senha" />
+              <input type="password" name="pass" id="pass"
+              onChange={(e)=> Setpass(e.target.value)}
+              placeholder="Senha" />
             </div>
           </div>
           <button type="submit" className="login-button">Login</button>
